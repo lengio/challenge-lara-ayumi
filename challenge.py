0be9452f2f0648951cfd22e7a5ceba2d98081ac6
Lara <laraayumi56@gmail.com>
2022-11-04 18:06:32 -0300

    Add basic json fetching and parsing

--- challenge.py
+++ challenge.py
@@ -1,17 +1,57 @@
 import requests
 import auth
+import example
 
 def apiGET():
-    return requests.get("https://api.slangapp.com/challenges/v1/activities", 
+    req = requests.get("https://api.slangapp.com/challenges/v1/activities", 
     headers={"Authorization": auth.KEY})
+    
+    return req
+    # status = req.status_code
+
+    # if (status == 200):
+    #     return req
+    # else:
+    #     print(status)
+    #     req.close()
+    #     exit()
+
+def apiGETDummy():
+    return example.EXAMPLE
 
 def apiPOST(user_sessions):
-    requests.post("https://api.slangapp.com/challenges/v1/activities/sessions",
-    headers={"Authorization": auth.KEY}, # ← replace with your key
-    json=user_sessions) # Keep in mind this should be a dictionary {“user_sessions”: {...}}
+    try:
+        requests.post("https://api.slangapp.com/challenges/v1/activities/sessions",
+        headers={"Authorization": auth.KEY}, # ← replace with your key
+        json=user_sessions) # Keep in mind this should be a dictionary {“user_sessions”: {...}}
+
+    except Exception as e:
+        print(e.args[0])
+        exit()
 
 def build_user_sessions(data):
-    print(data)
+    data_storage = {}
+    
+    # Linear time
+    for activity in data["activities"]:
+        obj = {
+            'id': activity['id'],
+            'answered_at': activity['answered_at'],
+            'first_seen_at': activity['first_seen_at'],
+        }
+
+        try:
+            q = data_storage[activity["user_id"]]
+        except KeyError:
+            q = []
+
+        q.append(obj)
+        data_storage[activity["user_id"]] = q
+    
+    print(data_storage)
 
 if __name__ == "__main__":
-    build_user_sessions('hello')
+    # activities_response = apiGET()
+    # build_user_sessions(activities_response.json())
+
+    build_user_sessions(apiGETDummy())
